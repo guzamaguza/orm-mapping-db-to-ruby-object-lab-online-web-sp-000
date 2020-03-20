5f75573e98f3a19b96f64fc663e39a0f975c1ac6
Mark A Guza <guzamaguza@gmail.com>
2020-03-20 03:50:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,9 @@ class Student
         LIMIT 1
     SQL
 
-    DB[:conn].execute(sql).first
+    DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)   #create a new student object
+    end
   end
 
   def self.all_students_in_grade_X(grade)
